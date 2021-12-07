d3b9df4134c04826808268753b233e82d6ead41b
Kristian <kristian.graul@gmail.com>
2021-12-07 13:00:39 +0100

    update package-name

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package de.lwsc.kristian;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'de.lwsc.kristian',
   appName: 'lwsc_simple',
   webDir: 'www',
   bundledWebRuntime: false
