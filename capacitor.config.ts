f1856ce48ad83c1b12ee3a0896564873ca791a63
Johannes Zinn <johannes@zinnchen.de>
2021-12-08 21:02:17 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lwsc-remote',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
